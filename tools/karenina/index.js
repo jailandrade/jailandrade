60767e12ed42233fa6b9e718b6537cbae1426efc
Mikhail Cruz Andrade <jailandrade@gmail.com>
2016-05-18 00:44:31 -0500

    install dependencies

--- tools/karenina/index.js
+++ tools/karenina/index.js
@@ -1,1 +1,29 @@
-var chalk = require('chalk');
+#! /usr/bin/env node
+
+var chalk 		= require('chalk'),
+    commander 	= require('commander'),
+    async 		= require('async'),
+    _ 			= require('underscore'),
+    debug 		= require('debug'),
+    bluebird 	= require('bluebird');
+
+// Ask for what kind of tool, generator or boilerplate is need to generate
+
+commander
+	.version('0.0.1')
+	.option('-g, --generators', 'Use generators')
+	.option('-b, --boilerplates', 'Use boilerplates')
+	.option('-t, --tools', 'Use tools')
+	.parse(process.argv);
+
+console.log('Hi Jail, what do you want use/create today');
+
+if (commander.generators) {
+	console.log('Using generators');
+}
+if (commander.boilerplates) {
+	console.log('Using boilerplates');
+}
+if (commander.tools) {
+	console.log('Using tools');
+}
